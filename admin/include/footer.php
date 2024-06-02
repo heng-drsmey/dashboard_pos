2f617d30c06691c68e621f13347536aeae48fad0
Odom133 <barnodomnew@gmail.com>
2024-06-03 00:05:49 +0700

    Update footer.php

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -1,3 +1,10 @@
+
+<!-- link jquert -->
+<!-- <script src="js/jquery.js"></script>
+<script src="js/custom.js"></script> -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="js/custom.js"></script>
+
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
