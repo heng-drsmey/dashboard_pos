8e41d236cc8240d6bd75913d12a8f201574440df
Samneang027 <154512847+Samneang027@users.noreply.github.com>
2024-06-06 13:41:51 +0700

    Update footer.php

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -1,3 +1,9 @@
+
+<!-- link jquert -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="js/alert_del.js"></script>
+
+
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
