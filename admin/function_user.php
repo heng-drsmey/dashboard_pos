bd1bb7c500152d17d6fb67bf576b1df3cda7a1ac
Odom133 <barnodomnew@gmail.com>
2024-06-07 00:02:16 +0700

    Update function_user.php

--- admin/function_user.php
+++ admin/function_user.php
@@ -8,7 +8,7 @@ function add_user()
   global $conn;
   if (isset($_POST['btnAdd'])) {
     $txtoutlet = $conn->real_escape_string($_POST['txtoutlet']);
-    $txttxtemp = $conn->real_escape_string($_POST['txtemp']);
+    $txtemp = $conn->real_escape_string($_POST['txtemp']);
     $txtrole = $conn->real_escape_string($_POST['txtrole']);
     $xtcreateby = $conn->real_escape_string($_POST['txtcreateby']);
     $txtusername = $conn->real_escape_string($_POST['txtusername']);
@@ -16,7 +16,7 @@ function add_user()
     $txtremark = $conn->real_escape_string($_POST['txtremark']);
 
     $sql = "INSERT INTO `user`(`OutletId`, `EmployeeId`, `RoleId`, `Username`, `Password`, `Remark`, `Status`, `CreateBy`) VALUES 
-    ('$txtoutlet','$txttxtemp','$txtrole','$txtusername','$txtpassword','$txtremark',1,'$xtcreateby')";
+                              ('$txtoutlet','$txtemp','$txtrole','$txtusername','$txtpassword','$txtremark',1,'$xtcreateby')";
     $rs = $conn->query($sql);
     if ($rs == true) {
       echo '
@@ -67,40 +67,45 @@ function delete_user()
 }
 
 
-// function update()
-// {
-//   global $conn;
-//   // selece data for update
-//   if (isset($_REQUEST['btnUpdate'])) {
-//     $uomId = $_REQUEST['Id'];
-//     $name = $conn->real_escape_string($_REQUEST['txtname']);
-//     $code = $conn->real_escape_string($_REQUEST['txtcode']);
-//     $remark = $conn->real_escape_string($_REQUEST['txtremark']);
-//     $curentDate = date("Y_m_d_H_i_s");
-//     $update_at = $_REQUEST['txtupdate_at'];
-//     $update = $update_at . $curentDate;
-//     $sqlUpdate = "UPDATE `uom` SET `Code`='$code',`Name`='$name',`Remark`='$remark',`UpdateAt`='$update' WHERE Id=$uomId";
-//     if ($conn->query($sqlUpdate) === TRUE) {
-//       echo '
-//                                 <script>
-//                                   swal({
-//                                     title: "Success",
-//                                     text: "Data insert success",
-//                                     icon: "success",
-//                                   });
-//                                 </script>
-//                                 ';
-//                                   } else {
-//                                     echo '
-//                                 <script>
-//                                   swal({
-//                                     title: "Try again",
-//                                     text: "Data can not insert",
-//                                     icon: "error",
-//                                   });
-//                                 </script>
-//                                 ';
-//     }
-//   }
-// }
+function update_user()
+{
+  global $conn;
+  // selece data for update
+  if (isset($_REQUEST['btnUpdate'])) {
+    $userId = $_REQUEST['Id'];
+    $txtoutlet = $conn->real_escape_string($_POST['txtoutlet']);
+    $txtemp = $conn->real_escape_string($_POST['txtemp']);
+    $txtrole = $conn->real_escape_string($_POST['txtrole']);
+    $xtcreateby = $conn->real_escape_string($_POST['txtcreateby']);
+    $txtusername = $conn->real_escape_string($_POST['txtusername']);
+    $txtpassword = $conn->real_escape_string($_POST['txtpassword']);
+    $txtremark = $conn->real_escape_string($_POST['txtremark']);
+    $curentDate = date("Y_m_d_H_i_s");
+    $update_at = $_REQUEST['txtupdate_at'];
+    $update = $update_at . $curentDate;
+
+    $sqlUpdate = "UPDATE `user` SET `OutletId`='$txtoutlet',`EmployeeId`='$txtemp',`RoleId`='$txtrole',`Username`='$txtusername',`Password`='$txtpassword',`Remark`='$txtremark',`CreateBy`='$xtcreateby',`UpdateAt`='$update' WHERE Id=$userId";
+    if ($conn->query($sqlUpdate) === TRUE) {
+      echo '
+                                <script>
+                                  swal({
+                                    title: "Success",
+                                    text: "Data Update success",
+                                    icon: "success",
+                                  });
+                                </script>
+                                ';
+    } else {
+      echo '
+                                <script>
+                                  swal({
+                                    title: "Try again",
+                                    text: "Data can not Update",
+                                    icon: "error",
+                                  });
+                                </script>
+                                ';
+    }
+  }
+}
 ?>
