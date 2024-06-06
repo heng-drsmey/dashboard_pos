0c66c6e95043bc0a1f86729d585ec66654367553
Odom133 <barnodomnew@gmail.com>
2024-06-07 00:01:52 +0700

    Update update-sql.sql

--- update-sql.sql
+++ update-sql.sql
@@ -1,6 +1,5 @@
 -- update (Jun-06-2024)
 ALTER TABLE `user` CHANGE `ActivedAt` `Remark` VARCHAR(255) NULL DEFAULT NULL;
-<<<<<<< HEAD
 -- update (Jun-06-2024)
 
 ALTER TABLE `customer` ADD `Remark` TEXT NULL AFTER `Address`;
@@ -11,7 +10,7 @@ ALTER TABLE `employeereviewsalary` ADD `Remark` TEXT NULL ;
 ALTER TABLE `invoice` ADD `Remark` TEXT NULL ;
 ALTER TABLE `outlet` ADD `Remark` TEXT NULL ;
 ALTER TABLE `shift` ADD `Remark` TEXT NULL ;
-=======
 --add columm logo on outlet
 ALTER TABLE `outlet` ADD `Logo` VARCHAR(255) NULL AFTER `Address`;
->>>>>>> 46c76eee1163fbe7a9ab8e7931397ef8b478bbce
+-- add column status on Table
+ALTER TABLE `table` ADD `Status` TINYINT NULL DEFAULT '1' AFTER `Description`;
