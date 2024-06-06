34c0db82c4cfd9e7c2eff8fcc1b61146dcc78053
Samneang027 <154512847+Samneang027@users.noreply.github.com>
2024-06-06 14:21:26 +0700

    Update update-sql.sql

--- update-sql.sql
+++ update-sql.sql
@@ -1,2 +1,4 @@
 -- update (Jun-06-2024)
 ALTER TABLE `user` CHANGE `ActivedAt` `Remark` VARCHAR(255) NULL DEFAULT NULL;
+--add columm logo on outlet
+ALTER TABLE `outlet` ADD `Logo` VARCHAR(255) NULL AFTER `Address`;
