c10e083e4bec8be03420500806c7caf5451c10b4
Odom133 <barnodomnew@gmail.com>
2024-06-06 17:28:28 +0700

    Update update-sql.sql

--- update-sql.sql
+++ update-sql.sql
@@ -1,2 +1,12 @@
 -- update (Jun-06-2024)
 ALTER TABLE `user` CHANGE `ActivedAt` `Remark` VARCHAR(255) NULL DEFAULT NULL;
+-- update (Jun-06-2024)
+
+ALTER TABLE `customer` ADD `Remark` TEXT NULL AFTER `Address`;
+
+ALTER TABLE `employee` ADD `Remark` TEXT NULL ;
+ALTER TABLE `employeepayroll` ADD `Remark` TEXT NULL ;
+ALTER TABLE `employeereviewsalary` ADD `Remark` TEXT NULL ;
+ALTER TABLE `invoice` ADD `Remark` TEXT NULL ;
+ALTER TABLE `outlet` ADD `Remark` TEXT NULL ;
+ALTER TABLE `shift` ADD `Remark` TEXT NULL ;
