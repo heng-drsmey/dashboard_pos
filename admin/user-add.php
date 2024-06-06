2567f58c6e010a705213c6adbda763d950123205
Odom133 <barnodomnew@gmail.com>
2024-06-07 00:01:59 +0700

    Update user-add.php

--- admin/user-add.php
+++ admin/user-add.php
@@ -47,41 +47,39 @@ include('function_user.php');
                         <h1 class="h3 mb-0 text-gray-800">Add User</h1>
                         <a href="user-list.php" class="d-none d-sm-inline-block btn btn-success shadow-sm">User List</a>
                     </div>
-                    <!-- form add product -->
+                    <!-- form add user -->
                     <div class="col-lg-12">
                         <div class="card shadow mb-4">
                             <div class="card-body">
                                 <form method="post" enctype="multipart/form-data">
                                     <?php
-                                    // call function add product
+                                    // call function add user
                                     add_user();
                                     // call data for update
-                                    // if (isset($_REQUEST['Id'])) {
-                                    //     $productId = $_REQUEST['Id'];                                   
-                                    //     // update();
-                                    //     $rowFrm = $conn->query("SELECT * FROM `productsku` WHERE Id=$productId")->fetch_assoc();
-                                    //     // $pro = $conn->query("SELECT * FROM `product` WHERE Id=" . $productId['ProductId'])->fetch_assoc();
-                                    // } else {
-                                    //     $rowFrm = array("Name" => "", "Code" => "", "Remark" => "",);
-                                    // }
+                                    if (isset($_REQUEST['Id'])) {
+                                        $userId = $_REQUEST['Id'];                                   
+                                        update_user();
+                                        $rowUser = $conn->query("SELECT * FROM `user` WHERE Id=$userId")->fetch_assoc();
+                                        // $pro = $conn->query("SELECT * FROM `user` WHERE Id=" . $userId['userId'])->fetch_assoc();
+                                    } else {
+                                        $rowUser = array("OutletId" => "", "EmployeeId" => "", "RoleId" => "","CreateBy" => "","Username" => "","Password" => "","Remark" => "",);
+                                    }
+                                    // echo var_dump($rowUser);
                                     ?>
-
                                     <div class="row">
                                         <div class="col-lg-12">
                                             <div class="card-body">
                                                 <div class="row">
                                                     <div class="col-lg-6">
                                                         <label for="Outlet">Outlet</label>
-                                                        <!-- <input type="text" class="form-control" name="txtoutlet" required value="<?php // echo '' . $ProId['Code'] . '' 
-                                                                                                                                        ?>"> -->
-                                                        <select class="form-control border-left-danger" name="txtoutlet">
+                                                        <select class="form-control border-left-danger" style="width: 100%;" name="txtoutlet">
                                                             <?php
-                                                            $sqlOutlet = "SELECT * FROM `outlet`";
-                                                            $qrOutlet = $conn->query($sqlOutlet);
-                                                            while ($rowOutlet = $qrOutlet->fetch_assoc()) {
-                                                                if ($rowOutlet['Id'] == $rowUser['Id'])  $sel = 'selected';
+                                                            $sqloutlet = "SELECT * FROM `outlet`";
+                                                            $qroutlet = $conn->query($sqloutlet);
+                                                            while ($rowoutlet = $qroutlet->fetch_assoc()) {
+                                                                if ($rowoutlet['Id'] == $rowUser['OutletId']) $sel = 'selected';
                                                                 else $sel = '';
-                                                                echo '<option value="' . $rowOutlet['Id'] . '" ' . $sel . '>' . $rowOutlet['Name'] . '</option>';
+                                                                echo '<option value="' . $rowoutlet['Id'] . '" ' . $sel . '>' . $rowoutlet['Name'] . '</option>';
                                                             }
                                                             ?>
                                                         </select>
@@ -93,7 +91,7 @@ include('function_user.php');
                                                             $sqlEmp = "SELECT * FROM `employee`";
                                                             $qrEmp = $conn->query($sqlEmp);
                                                             while ($rowEmp = $qrEmp->fetch_assoc()) {
-                                                                if ($rowEmp['Id'] == $rowUser['Id'])  $sel = 'selected';
+                                                                if ($rowEmp['Id'] == $rowUser['EmployeeId'])  $sel = 'selected';
                                                                 else $sel = '';
                                                                 echo '<option value="' . $rowEmp['Id'] . '" ' . $sel . '>' . $rowEmp['Firstname'] . ' ' . $rowEmp['Lastname'] . '</option>';
                                                             }
@@ -109,7 +107,7 @@ include('function_user.php');
                                                             $sqlRole = "SELECT * FROM `role`";
                                                             $qrRole = $conn->query($sqlRole);
                                                             while ($rowRole = $qrRole->fetch_assoc()) {
-                                                                if ($rowRole['Id'] == $rowFrm['Id']) $sel = 'selected';
+                                                                if ($rowRole['Id'] == $rowUser['RoleId']) $sel = 'selected';
                                                                 else $sel = '';
                                                                 echo '<option value="' . $rowRole['Id'] . '" ' . $sel . '>' . $rowRole['Name'] . '</option>';
                                                             }
@@ -121,12 +119,12 @@ include('function_user.php');
                                                         <label for="CreateBy">CreateBy</label>
                                                         <select class="form-control border-left-danger" style="width: 100%;" name="txtcreateby">
                                                             <?php
-                                                            $sqlUser = "SELECT * FROM `user`";
-                                                            $qrUser = $conn->query($sqlUser);
-                                                            while ($rowUser = $qrUser->fetch_assoc()) {
-                                                                if ($rowUser['Id'] == $rowFrm['Id']) $sel = 'selected';
+                                                            $sqlUser1 = "SELECT * FROM `user`";
+                                                            $qrUser1 = $conn->query($sqlUser1);
+                                                            while ($rowUser1 = $qrUser1->fetch_assoc()) {
+                                                                if ($rowUser1['Id'] == $rowUser['CreateBy']) $sel = 'selected';
                                                                 else $sel = '';
-                                                                echo '<option value="' . $rowUser['Id'] . '" ' . $sel . '>' . $rowUser['Username'] . '</option>';
+                                                                echo '<option value="' . $rowUser1['Id'] . '" ' . $sel . '>' . $rowUser1['Username'] . '</option>';
                                                             }
 
                                                             ?>
@@ -136,22 +134,17 @@ include('function_user.php');
                                                 <div class="row">
                                                     <div class="col-lg-6">
                                                         <label for="Username">Username</label>
-                                                        <input type="text" class="form-control border-left-danger" name="txtusername" required value="">
+                                                        <input type="text" class="form-control border-left-danger" name="txtusername" required value="<?php echo''.$rowUser['Username'].''; ?>">
                                                     </div>
                                                     <div class="col-lg-6">
                                                         <label for="Password">Password</label>
-                                                        <input type="text" class="form-control border-left-danger" name="txtpassword" required value="<?php echo '' //  .$rowFrm['ProductId'] . '' ?>">
+                                                        <input type="text" class="form-control border-left-danger" name="txtpassword" required value="<?php echo '' .$rowUser['Password'] . '' ?>">
                                                     </div>
                                                 </div>
                                                 <label for="Remark">Remark</label>
-                                                <input type="text" class="form-control" name="txtremark">
+                                                <input type="text" class="form-control" name="txtremark" value="<?php echo ''.$rowUser['Remark'].'' ?>">
 
-
-                                                <input style="display: none;" type="text" class="form-control" name="txtupdate_at">
-                                                <!-- <div class="form-check form-switch ms-4 mt-3">
-                                                    <input class="form-check-input" type="checkbox" role="switch" id="status" name="txtstatus">
-                                                    <label class="form-check-label mb-2 " for="status">Disable</label>
-                                                </div> -->
+                                                <input style="display: none;" type="text" class="form-control" name="txtupdate_at" >
                                                 <?php
                                                 if (isset($_REQUEST['Id'])) {
                                                     echo '
@@ -166,10 +159,7 @@ include('function_user.php');
                                                 ?>
                                             </div>
                                         </div>
-
-
                                     </div>
-
                                 </form>
                                 <!-- end form  -->
                             </div>
