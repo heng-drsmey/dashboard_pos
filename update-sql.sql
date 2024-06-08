d56d1a7dc02fab57dd0f33746e3fef30e90fe1c5
Odom133 <barnodomnew@gmail.com>
2024-06-08 13:12:57 +0700

    Update update-sql.sql

--- update-sql.sql
+++ update-sql.sql
@@ -19,3 +19,19 @@ ALTER TABLE `table` ADD `Status` TINYINT NULL DEFAULT '1' AFTER `Description`;
 ALTER TABLE `paymentmethod` 
 ADD FOREIGN KEY (CreateBy) REFERENCES `user` (Id);
 ALTER TABLE `currency` ADD `Status` TINYINT NOT NULL DEFAULT '1' AFTER `CreateBy`;
+-- add column add on table pro_in (Jun-08-2024: 12:05AM)
+ALTER TABLE `pro_in` ADD `PurchaseNo` VARCHAR(20) NULL AFTER `Id`;
+ALTER TABLE `pro_in` ADD `DiscountAmount` DECIMAL(10,2) NULL DEFAULT '00.00' AFTER `Price_In`;
+ALTER TABLE `pro_in` ADD `RecieveDate` DATE NULL AFTER `Id`;
+ALTER TABLE `pro_in` CHANGE `Description` `Description` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
+ALTER TABLE `pro_in` ADD `RecieveBy` TINYINT NULL AFTER `RecieveDate`;
+ALTER TABLE `pro_in` ADD `Supplier` TINYINT NULL AFTER `RecieveBy`;
+ALTER TABLE `pro_in` ADD `Paid` DECIMAL(10,2) NULL AFTER `Description`;
+ALTER TABLE `pro_in` ADD `PaymentStatus` TINYINT NULL AFTER `Paid`;
+
+INSERT INTO `supplier`( `Name`, `Email`, `Phone_Number`, `Address`, `CreateBy`) VALUES ('General Supplier',' ',0987654321,'Cambodia','1');
+
+
+-- add foriend key add on table supplier (Jun-08-2024: 12:05AM)
+ALTER TABLE `supplier` ADD FOREIGN KEY (CreateBy) REFERENCES `user` (Id);
+ALTER TABLE `pro_in` ADD FOREIGN KEY (Uom) REFERENCES `uom` (Id);
