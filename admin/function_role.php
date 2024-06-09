2ca2271cdf53b4bcd980b23ee9d8779217915b03
Odom133 <barnodomnew@gmail.com>
2024-06-10 00:20:51 +0700

    Update function_role.php

--- admin/function_role.php
+++ admin/function_role.php
@@ -43,17 +43,16 @@ function delete()
   global $conn;
   if (isset($_GET['delId'])) {
     $delId = mysqli_real_escape_string($conn, $_GET['delId']);
-    $sqlDeleterole = "DELETE FROM `role` WHERE `Id`='$delId'";
+    $sqlDeleterole = "UPDATE `role` SET del=0 WHERE `Id`='$delId'";
     if ($conn->query($sqlDeleterole) == TRUE) {
       echo '
-                <script>
-                swal({
-                    title: "Success",
-                    text: "Data delete success",
-                    icon: "success",
-                });
-                </script> 
-    ';
+        <div class="alert alert-success alert-dismissible fade show" role="alert" id="alert-success">
+          <strong>Delete Success.</strong>
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      ';
     } else {
       echo "Error deleting record: " . $conn->error;
     }
