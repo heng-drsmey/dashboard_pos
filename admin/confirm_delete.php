e9ce7b0e51a4ad3d58086d5ec523e2bc73d541b3
Odom133 <barnodomnew@gmail.com>
2024-06-10 17:01:01 +0700

    Update confirm_delete.php

--- admin/confirm_delete.php
+++ admin/confirm_delete.php
@@ -2,20 +2,20 @@
 
 <!-- Modal -->
 <div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="confirm-deleteTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header bg-info">
-        <h5 class="modal-title text-dark" id="Title">After delete you can not restore!</h5>
+  <div class="modal-dialog  modal-dialog-centered " role="document">
+    <div class="modal-content ">
+      <div class="modal-header  ">
+        <h5 class="modal-title " id="Title">After delete you can not restore!</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body text-danger">
-        Are you sure you want to delete ?
+      <div class="modal-body text-danger " >
+        <b>Are you sure you want to delete ?</b>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <a class="btn btn-primary btn-ok">YES</a>
+        <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Close</button>
+        <a class="btn btn-primary btn-ok btn-sm">YES</a>
       </div>
     </div>
   </div>
